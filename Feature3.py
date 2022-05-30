5277a887b63bbf636ed58b693912fcf4527b2ea1
daniazzam <dani.azzam@lau.edu>
2022-05-30 20:09:21 +0300

    updated main/feature3

--- Feature3.py
+++ Feature3.py
@@ -2,18 +2,21 @@
 #Feature #3: automatic gate using distance sensor+Buzzer+SERVO
 ############################################################
 
-from curses import echo
 from gpiozero import DistanceSensor, Buzzer, Servo
 import RPi.GPIO as GPIO
 from time import sleep
+import datetime
 
+with open('readme.txt', 'w') as f:
+    f.write('readme')
 
 class Feature3:
-    def __init__(self):
+    def __init__(self,directory):
         self.distSensor=DistanceSensor(echo=3 ,trigger=2 ,threshold_distance=1)
         self.servo=Servo(24)
         self.gateBuzzer=Buzzer(12)
         self.distance=1
+        self.F=open(directory,"w")
 
     def checkDistance(self):
 
@@ -27,6 +30,7 @@ class Feature3:
                 pass
             else:
                 #open the gate using servo by setting it to its mid position 90 degrees
+                self.F.write("The gate opened at "+str(datetime.datetime.now()))
                 self.servo.mid()
                 for i in range(4):
                     self.gateBuzzer.on()
@@ -42,5 +46,5 @@ class Feature3:
         self.checkDistance()
 
 if __name__=='__main__':
-    F3=Feature3()
+    F3=Feature3(r'/home/pi/Desktop/GateInfo.txt')
     F3.run_feature3()
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import Feature1,Feature2,Feature3,Feature4
 
-F1=Feature1
-F2=Feature2
-F3=Feature3
-F4=Feature4
+F1=Feature1.Feature1()
+F2=Feature2.Feature2()
+F3=Feature3.Feature3(r'/home/pi/Desktop/GateInfo.txt')
+F4=Feature4.Feature4()
 
 while True:
     F1.run_Feature1()
